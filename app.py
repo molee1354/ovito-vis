bd45c0888605bcfa0a4ffdd09ccc84cc808b5f2f
molee1354 <molee1354@gmail.com>
2022-10-25 17:28:18 -0400

    embedded as widgets in main window

--- app.py
+++ app.py
@@ -1,37 +1,67 @@
-import sys, os
-from time import sleep
-# Future versions of OVITO will use Qt6 & PySide6, older versions use Qt5 & PySide2.
-# Please pick the right PySide version for import.
-from PySide6.QtCore import QEventLoop, QTimer
-from PySide6.QtWidgets import QApplication
+import sys
 
-myapp = QApplication(sys.argv)
-
-# Note: Import the ovito package AFTER the QApplication object
-# has been created. Otherwise, Ovito would automatically create its own 
-# QCoreApplication object, which won't let us display GUI widgets.
-from ovito.io import import_file
-from ovito.vis import Viewport
-
-# Import model and add it to visualization scene.
 import local_paths
-pipeline = import_file(local_paths.filename)
-pipeline.add_to_scene()
 
-# Create a virtual Viewport.
-vp = Viewport(type=Viewport.Type.Perspective, camera_dir=(0, 0, -1))
-vp.camera_pos = (0.160,0.2,0.55)
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import (
+    QHBoxLayout,
+    QApplication, QMainWindow, QPushButton, QWidget)
 
-# Create a visible GUI widget associated with the viewport.
-widget = vp.create_qt_widget()
-widget.resize(640, 480)
-widget.setWindowTitle("Random Impact Player")
-widget.show()
-sleep(1.0)
-vp.dataset.anim.start_animation_playback(3.)
 
-# Shut down application when the user closes the viewport widget.
-widget.destroyed.connect(QApplication.instance().quit)
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
 
-# sys.exit(myapp.exec_()) 
-sys.exit(myapp.exec())
+        self.setWindowTitle("Simulation Player")
+        self.setFixedSize( QSize(800,600) )
+        layout = QHBoxLayout()
+
+        self.viewport = self.ovito_viewport(local_paths.bennus)
+
+        self.sim_window = self.viewport.create_qt_widget()
+        button = QPushButton("Button")
+        button.setFixedSize( QSize(200,100))
+
+        layout.addWidget(self.sim_window)
+        layout.addWidget(button)
+
+        widg = QWidget()
+        widg.setLayout(layout)
+        self.setCentralWidget(widg)
+        # self.setCentralWidget(self.widget)
+
+    def ovito_viewport(self, filename: str):
+        from ovito.io import import_file
+        from ovito.vis import Viewport
+
+        pipeline = import_file(filename)
+        pipeline.add_to_scene()
+
+        out = Viewport(type=Viewport.Type.Perspective, camera_dir=(0,0,-1))
+        out.camera_pos = (0.16,0.2,0.55)
+
+        return out
+
+    def do(self) -> None:
+        self.sim_window.show()
+        
+        self.viewport.dataset.anim.start_animation_playback(3.)
+
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    window.do()
+    # window.widget.resize(640,480)
+    # window.widget.setWindowTitle("Random Impact Player")
+    # window.widget.show()
+    
+    # window.vp.dataset.anim.start_animation_playback(3.)
+
+    window.show()
+
+    app.exec()
+
+if __name__ == "__main__":
+    main()
