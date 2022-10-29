2a114098fc160891b5fe76cb24afeeb47387d622
molee1354 <molee1354@gmail.com>
2022-10-29 13:24:23 -0400

    reset mechanism working

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 import sys
-import random
-
+from random import shuffle
 import local_paths
+
 
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import (
@@ -17,14 +17,20 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Simulation Player")
         self.setFixedSize( QSize(800,600) )
-        self.layout1 = QHBoxLayout()
+        
+        # layouts
+        layout1 = QHBoxLayout()
         layout2 = QVBoxLayout()
 
-        self.file = f"{local_paths.files_dir}/{random.choice(os.listdir(local_paths.files_dir))}"
+        # simulations
+        # self.file = f"{local_paths.files_dir}/{random.choice(os.listdir(local_paths.files_dir))}"
+
+        self.files_list = self.generate_file()
+        self.file = next(self.files_list)
         self.viewport = self.ovito_viewport(self.file)
 
         self.sim_window = self.viewport.create_qt_widget()
-        self.layout1.addWidget(self.sim_window)
+        layout1.addWidget(self.sim_window)
         self.sim_window.destroyed.connect(QApplication.instance().quit)
 
         button = QPushButton("Button")
@@ -39,24 +45,44 @@ class MainWindow(QMainWindow):
         layout2.addWidget(button)
         layout2.addWidget(inputbox)
 
-        self.layout1.addLayout(layout2)
+        layout1.addLayout(layout2)
 
-        widget1 = QWidget()
-        widget1.setLayout(self.layout1)
-        self.setCentralWidget(widget1)
+        widget = QWidget()
+        widget.setLayout(layout1)
+        self.setCentralWidget(widget)
+
+    def generate_file(self) -> str:
+        files_list = [ f"{local_paths.files_dir}{f}"
+                       for f in os.listdir(local_paths.files_dir) ]
+        shuffle(files_list)
+        for i,file in enumerate(files_list):
+            print(i)
+            yield file
 
     def ovito_viewport(self, filename: str):
-        from ovito.io import import_file
         from ovito.vis import Viewport
+        from ovito.io import import_file
 
-        pipeline = import_file(filename)
-        pipeline.add_to_scene()
+        self.pipeline = import_file(filename)
+        self.pipeline.add_to_scene()
 
         out = Viewport(type=Viewport.Type.Perspective)
         out.camera_dir=(0,0,-1)
         out.camera_pos = (0.16,0.2,0.55)
 
         return out
+    
+    def reset_viewport(self) -> None:
+        from ovito.io import import_file
+        self.pipeline.remove_from_scene()
+
+        # filename = f"{local_paths.files_dir}/{random.choice(os.listdir(local_paths.files_dir))}"
+        self.file = next(self.files_list)
+        print(self.file)
+        self.pipeline = import_file(self.file)
+        self.pipeline.add_to_scene()
+
+        self.viewport.dataset.anim.start_animation_playback(3.)
 
     def play(self) -> None:
         #TODO self.layout1.removeWidget(self.sim_window)
@@ -64,7 +90,7 @@ class MainWindow(QMainWindow):
         self.viewport.dataset.anim.start_animation_playback(3.)
     
     def return_pressed(self) -> None:
-        print("return pressed")
+        self.reset_viewport()
 
 
 def main() -> None:
@@ -72,7 +98,7 @@ def main() -> None:
     window = MainWindow()
     window.show()
 
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
