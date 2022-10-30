41adc6108e9967f39288339d05c248d9ba562395
molee1354 <molee1354@gmail.com>
2022-10-29 20:05:04 -0400

    fix app keyword

--- setup.bat
+++ setup.bat
@@ -1,13 +1,10 @@
-@echo off
 FOR /F "tokens=* USEBACKQ" %%F IN (`where python ^| findstr Python310`) DO (
 SET py310=%%F
 )
 IF [%py310%]==[] SET py310=py
 
-%py310% -m virtualenv venv && venv\Scripts\activate.bat && pip install -r packages.txt
+%py310% -m virtualenv venv && venv\Scripts\activate.bat && pip install -r packages.txt && DOSKEY app=py app.py $*
 if %ERRORLEVEL% neq 0 GOTO ProcessError
 
 :ProcessError
-%py310% -m venv venv && venv\Scripts\activate.bat && pip install -r packages.txt
-
-DOSKEY app=py app.py $*
+%py310% -m venv venv && venv\Scripts\activate.bat && pip install -r packages.txt && DOSKEY app=py app.py $*
